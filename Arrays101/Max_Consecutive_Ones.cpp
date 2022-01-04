bb9af953ba1e534b6e264a1d466a48b7cb04458c
Rachit Garg <rachitgarg900@gmail.com>
2022-01-04 19:45:36 +0100

    Update Max_Consecutive_Ones.cpp

--- Arrays101/Max_Consecutive_Ones.cpp
+++ Arrays101/Max_Consecutive_Ones.cpp
@@ -1,1 +1,29 @@
-
+class Solution {
+public:
+    int findMaxConsecutiveOnes(vector<int>& nums) {
+        
+        int maxOnes = 0;
+        int ones = 0;
+        
+        for (int i = 0; i < nums.size(); i++) {
+            
+            if (nums[i] == 0) {
+                
+                if (ones > maxOnes) {
+                    maxOnes = ones;
+                }
+                ones = 0;
+                
+            } else {
+                ones++;
+            }
+        }
+        
+        if (ones > maxOnes) {
+            maxOnes = ones;
+        }
+        
+        return maxOnes;
+        
+    }
+};
